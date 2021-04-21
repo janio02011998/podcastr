176558d68abe4db7ed4bf4703798e8d51249e2e2
Janio <carvalho02011998@gmail.com>
2021-04-21 19:24:36 -0300

    SSG concepts and more  styles

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -14,9 +14,35 @@ export function Player() {
             <div className={styles.emptyPlayer}>
                 <strong>Selecione um podcast para ouvir</strong>
             </div>
-            <footer>
+            <footer className={styles.empty}>
+                <div className={styles.progress}>
+                    <span>00:00</span>
+                    <div className={styles.slider}>
+                        <div className={styles.emptySlider} />
+                    </div>
+                    <span>00:00</span>
+                </div>
 
+                <div className={styles.buttons}>
+                    <button type='button'>
+                        <img src='/assets/shuffle.svg' alt="Embaralhar" />
+                    </button>
+
+                    <button type='button'>
+                        <img src='/assets/play-previous.svg' alt="Tocar anterior" />
+                    </button>
+                    <button type='button' className={styles.playButton}>
+                        <img src='/assets/play.svg' alt="Tocar" />
+                    </button>
+
+                    <button type='button'>
+                        <img src='/assets/play-next.svg' alt="Tocar próxima" />
+                    </button>
+                    <button type='button'>
+                        <img src='/assets/repeat.svg' alt="Repetir" />
+                    </button>
+                </div>
             </footer>
-        </div>
+        </div >
     );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,22 @@
 import React from 'react'
 
-export default class Home extends React.Component {
-  render() {
-    return (
-      <div>
-        Teste
-      </div>
-    );
+export default function Home(props) {
+
+  return (
+    <div>
+      <p>{JSON.stringify(props.episodes)}</p>
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  const response = await fetch('http://localhost:3333/episodes');
+  const data = await  response.json();
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
   }
 }
